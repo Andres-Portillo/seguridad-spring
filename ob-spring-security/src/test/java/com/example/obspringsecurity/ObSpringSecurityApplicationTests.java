9301a268a954a8f16be5c388d88c114cd7d54b43
andresportillo <andresjavierportillo22@gmail.com>
2022-10-21 14:25:25 -0500

    Initial commit

---
+++ ob-spring-security/src/test/java/com/example/obspringsecurity/ObSpringSecurityApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.obspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ObSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        System.getenv().forEach((key, value) -> System.out.println(key + " - " + value));
+    }
+
+}
